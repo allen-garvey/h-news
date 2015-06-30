e50698673aa0c71d7160bb18f3f84bcd563e2881
Allen Garvey <allengarvey@hotmail.com>
2015-06-30 01:04:09 -0400

    add routing for comments

--- index.php
+++ index.php
@@ -6,10 +6,16 @@ require_once(CONTROLLERS_PATH.'page_controller.php');
 if(isset($_GET['page'])){
 	if($_GET['page'] == 'ask'){
 		$page_controller = new HNewsAskPageController;
+		$page = 'ask';
 	}
 	elseif($_GET['page'] == 'show'){
 		$page_controller = new HNewsShowPageController;
+		$page = 'show';
 	}
+}
+if (isset($_GET['comments'])) {
+	$comments_for = isset($page) ? $page : null;
+	$page_controller = new HNewsCommentsPageController((int) $_GET['comments'], $comments_for);
 }
 
 if(!isset($page_controller)){
