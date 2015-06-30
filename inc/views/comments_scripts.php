2bf9d72237450e022c89329d5d0008b230b0dae4
Allen Garvey <allengarvey@hotmail.com>
2015-06-30 11:42:44 -0400

    add js initial pageName variable so ask pages are displayed correctly

--- inc/views/comments_scripts.php
+++ inc/views/comments_scripts.php
@@ -1,2 +1,2 @@
 <script type="text/javascript">var HN={};HN.storiesUrl = '<?= $page_controller->getStoryIdsUrl(); ?>';
-HN.pageType='<?= $page_controller->getControllerType(); ?>';</script>
+HN.pageType='<?= $page_controller->getControllerType(); ?>';HN.pageName='<?= $page_controller->getTitle(); ?>';</script>
--- inc/views/story_scripts.php
+++ inc/views/story_scripts.php
@@ -2,4 +2,5 @@
 HN.pageType='<?= $page_controller->getControllerType(); ?>';
 HN.storiesPerPage = <?= $page_controller->getNumStoriesPerPage(); ?>;
 HN.shouldDisplayStory = <?= $page_controller->displayStoryFunction(); ?>;
-HN.commentsUrl = '<?= $page_controller->getCommentsUrl(); ?>';</script>
+HN.commentsUrl = '<?= $page_controller->getCommentsUrl(); ?>';
+HN.pageName='<?= $page_controller->getTitle(); ?>';HN.askUrl='<?= ASK_COMMENTS_QUERY_URL; ?>';</script>
