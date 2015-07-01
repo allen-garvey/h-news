ef9db3bd828076f7104bd6bad68e959d3d4362e8
Allen Garvey <allengarvey@hotmail.com>
2015-06-30 21:57:30 -0400

    refactor view so the correct app.min.js file is served in non development environments

--- inc/views/page.php
+++ inc/views/page.php
@@ -41,10 +41,10 @@
                 include(VIEWS_PATH.'story_scripts.php');
             }
          ?>
-         <?php if(ENVIRONMENT_CURRENT === ENVIRONMENT_PRODUCTION): ?>
-            <script src="<?= SCRIPTS_URL.'app.min.js'; ?>" type="text/javascript"></script>
-        <?php else: ?>
+         <?php if(ENVIRONMENT_CURRENT === ENVIRONMENT_DEVELOPMENT): ?>
             <script src="<?= SCRIPTS_URL.'app.js'; ?>" type="text/javascript"></script>
+        <?php else: ?>
+            <script src="<?= SCRIPTS_URL.'app.min.js'; ?>" type="text/javascript"></script>
         <?php endif; ?>
     </body>
 </html>
