e7bbdc53c0bd0c3b0e9dd82957901b52ca5ee674
Allen Garvey <allengarvey@hotmail.com>
2015-07-05 00:11:38 -0400

    edit page description and add style so footer stays at the bottom of the page

--- inc/views/page.php
+++ inc/views/page.php
@@ -3,27 +3,28 @@
     <head>
         <meta charset="utf-8"/>
         <title>H-News</title>
-        <meta name="description" content="H News, a mobile friendly version of your favorite programming news site"/>
+        <meta name="description" content="H News, a mobile friendly version of your favorite programming and start-up news site"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" type="text/css" href="<?= STYLES_URL.'style.css'; ?>"/>
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     </head>
     <body>
-        <header class='jumbotron'>
-            <div class='container'>
-                <nav>
-                    <a href='<?= HOME_URL; ?>'><span class='brand <?php if($page_controller->getTitle()==='home'){echo 'current';} ?>'>H-News</span></a>
-                    <ul>
-                        <a href='<?= SHOW_URL; ?>'><li<?php if($page_controller->getTitle()==='show'){echo ' class="current"';} ?>>Show</li></a>
-                        <a href='<?= ASK_URL; ?>'><li<?php if($page_controller->getTitle()==='ask'){echo ' class="current"';} ?>>Ask</li></a>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>
-            <ol id='top_list'>
-            </ol>
-        </main>
+        <div class='total'>
+            <header class='jumbotron'>
+                <div class='container'>
+                    <nav>
+                        <a href='<?= HOME_URL; ?>'><span class='brand <?php if($page_controller->getTitle()==='home'){echo 'current';} ?>'>H-News</span></a>
+                        <ul>
+                            <a href='<?= SHOW_URL; ?>'><li<?php if($page_controller->getTitle()==='show'){echo ' class="current"';} ?>>Show</li></a>
+                            <a href='<?= ASK_URL; ?>'><li<?php if($page_controller->getTitle()==='ask'){echo ' class="current"';} ?>>Ask</li></a>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main>
+                <ol id='top_list'>
+                </ol>
+            </main>
+        </div>
         <footer>
             <div class='container'>
                 <form method='GET' action='https://hn.algolia.com'>
