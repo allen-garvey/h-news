a67936e8adbf6e6e538d5bbd2fbcb172eef1d1b1
Allen Garvey <allengarvey@hotmail.com>
2015-07-09 02:52:19 -0400

    add js script variable inititalizer for settings page

---
+++ inc/views/settings_scripts.php
@@ -0,0 +1,3 @@
+<script type="text/javascript">var HN={};HN.pageType='settings';HN.settings = {};
+HN.settings.themeNames=<?= json_encode($page_controller->getThemeList()); ?>;
+HN.settings.themeIdPrefix='<?= $page_controller->getThemeIdPrefix(); ?>';</script>
