2134f8ec17ac14394727972c6b069ae02d39dac3
Allen Garvey <allengarvey@hotmail.com>
2015-09-26 23:41:51 -0400

    fix bug where stories local to hnews were displaying empty parentheses after story title

--- js/hn_util.js
+++ js/hn_util.js
@@ -26,7 +26,12 @@ HN.util.getItemInfoUrlFromId = function(id){
 * Takes HN.Story object as a parameter and returns the html to display the title and link
 */
 HN.util.getStoryTitleHTML = function(story){
-	return "<a href='" + story.url() +"'><h3>" + story.title() + "<span class='small url_source'> "+ story.urlRoot() + "</span></h3></a>";
+	var title = "<a href='" + story.url() +"'><h3>" + story.title();
+	if(story.urlRoot() !== ''){
+		title += "<span class='small url_source'> " + story.urlRoot() + "</span>";
+	}
+	title += "</h3></a>";
+	return title;
 };
 
 /**
