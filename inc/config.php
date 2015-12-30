bf3051f78669a4f90d3eed1dbd0c3424117e7290
Allen Garvey <allengarvey@hotmail.com>
2015-12-29 22:23:45 -0500

    refactor to not need to set current env to set root path

--- inc/config.php
+++ inc/config.php
@@ -1,22 +1,23 @@
 <?php 
 define('ENVIRONMENT_DEVELOPMENT', 0);
 define('ENVIRONMENT_HNEWS_CO', 2);
-require_once('current_environment.php');
 
-if(ENVIRONMENT_CURRENT === ENVIRONMENT_HNEWS_CO){
-	define('BASE_URL','http://www.hnews.co/');
-	define('COOKIE_URL', 'hnews.co');
-	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/');
-}
-else{
-	define('BASE_URL','/h-news/');
-	define('COOKIE_URL', BASE_URL);
-	define('ROOT_PATH',$_SERVER['DOCUMENT_ROOT'] . '/h-news/');	
-}
+define('ROOT_PATH', dirname(__FILE__, 2).'/');
 define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
 define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 define('MODELS_PATH', INC_PATH.'models/');
+
+require_once(INC_PATH.'current_environment.php');
+
+if(ENVIRONMENT_CURRENT === ENVIRONMENT_HNEWS_CO){
+	define('BASE_URL','http://www.hnews.co/');
+	define('COOKIE_URL', 'hnews.co');
+}
+else{
+	define('BASE_URL','/h-news/');
+	define('COOKIE_URL', BASE_URL);
+}
 
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
