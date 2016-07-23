5047be412aa24aa746e6f5c5ef67a737e5f554bc
Allen Garvey <allengarvey@hotmail.com>
2016-07-22 20:05:16 -0400

    Fix bug where after hash change event on comments page, new comments would be added to existing comments on page, instead of replacing them

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ HN.settings.initSettings();
 
 if(HN.pageType === 'comments'){
 	//add listener for if user clicks on comments link while on comments page
-	window.addEventListener('hashchange', HN.displayComments, false);
+	window.addEventListener('hashchange', function(){HN.clearComments(); HN.displayComments();}, false);
 	HN.displayComments();
 }
 else if(HN.pageType === 'settings'){
