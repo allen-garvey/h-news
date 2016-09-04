1ffbcb19a390590cb8559e0fb2a6df0eab5c7893
Allen Garvey <allengarvey@hotmail.com>
2016-09-03 23:00:17 -0400

    Remove unused variable

--- inc/views/story_scripts.php
+++ inc/views/story_scripts.php
@@ -1,3 +1,2 @@
 HN.shouldDisplayStory = <?= $page_controller->displayStoryFunction(); ?>;
 HN.commentsUrl = '<?= $page_controller->getCommentsUrl(); ?>';
-HN.pageName='<?= $page_controller->getTitle(); ?>';
