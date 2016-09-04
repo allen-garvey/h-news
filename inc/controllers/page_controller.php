a885657af289f09bee040ccadb853d1e63913dd7
Allen Garvey <allen@allengarvey.com>
2016-09-04 11:15:04 -0400

    Remove need for page controllers since all page specific settings are controlled by js routing

--- inc/controllers/page_controller.php
+++ inc/controllers/page_controller.php
@@ -1,107 +1,14 @@
 <?php 
-/**
-* Controls such things as title of the page, which kinds of stories are displayed
-*/
-interface HNewsPage{
-	/**
-	* Title of the page, used to display navigation
-	*/
-	public function getTitle();
-	public function getControllerType();
-	public function getPageTitle();
-	
-}
 
 /**
-* Controls such things as initial the settings, which is just visual themes for now
+* Constants for saved user settings
 */
-class HNewsSettingsPageController implements HNewsPage{
+class HNewsSettingsPageController{
 	public static $userThemeFormName = "userTheme";
 	public static $autoDarkModeFormName = "autoDark";
 	public static $themeIdPrefix = "theme_";
 	public static function getThemeList(){
 		return ['default', 'dark', 'blue', 'green', 'purple', 'silver', 'red'];
 	}
-	public function getTitle(){
-		return 'settings';
-	}
-	public function getControllerType(){
-		return 'settings';
-	}
-	public function getPageTitle(){
-		return PAGE_TITLE_DEFAULT.'&mdash;Settings';
-	}
 
-}
-
-/**
-* Controls such things as title of the page, and the parent id of the comments displayed
-*/
-class HNewsCommentsPageController implements HNewsPage{
-	protected $title;
-	
-	function __construct($title=null) {
-		if(isset($title) & in_array($title, ['show', 'ask'])){
-			$this->title = $title;
-		}
-		else{
-			$this->title = 'home';
-		}
-	}
-	/**
-	* Title of the page, used to display navigation
-	*/
-	public function getTitle(){
-		return $this->title;
-	}
-	public function getPageTitle(){
-		return PAGE_TITLE_DEFAULT;
-	}
-	/**
-	* Used to retrieve json about parent story and get its comment ids
-	*/
-	public function getStoryIdsUrl(){
-		return 'https://hacker-news.firebaseio.com/v0/item/' . $this->parentStoryId . '.json';
-	}
-	public function getControllerType(){
-		return 'comments';
-	}
-	
-}
-
-/**
-* Controls such things as title of the page, which kinds of stories are displayed
-* for stories (homepage, ask and show main pages)
-*/
-abstract class HNewsAbstractStoryController implements HNewsPage{
-	public function getPageTitle(){
-		return PAGE_TITLE_DEFAULT;
-	}
-	public function getControllerType(){
-		return 'story';
-	}
-}
-
-class HNewsHomePageController extends HNewsAbstractStoryController{
-	public function getTitle(){
-		return 'home';
-	}
-}
-
-class HNewsShowPageController extends HNewsAbstractStoryController{
-	public function getTitle(){
-		return 'show';
-	}
-	public function getPageTitle(){
-		return PAGE_TITLE_DEFAULT.'&mdash;Show';
-	}
-}
-
-class HNewsAskPageController extends HNewsAbstractStoryController{
-	public function getTitle(){
-		return 'ask';
-	}
-	public function getPageTitle(){
-		return PAGE_TITLE_DEFAULT.'&mdash;Ask';
-	}
 }
--- public_html/index.php
+++ public_html/index.php
@@ -2,27 +2,6 @@
 require_once('../inc/config.php');
 require_once(CONTROLLERS_PATH.'page_controller.php');
 
-
-if(isset($_GET['comments'])) {
-	$comments_for = isset($_GET['page']) ? $_GET['page'] : null;
-	$page_controller = new HNewsCommentsPageController($comments_for);
-}
-else if(isset($_GET['page'])){
-	if($_GET['page'] == 'settings'){
-		$page_controller = new HNewsSettingsPageController;
-	}
-	elseif($_GET['page'] == 'ask'){
-		$page_controller = new HNewsAskPageController;
-	}
-	elseif($_GET['page'] == 'show'){
-		$page_controller = new HNewsShowPageController;
-	}
-}
-
-if(!isset($page_controller)){
-	$page_controller = new HNewsHomePageController;
-}
-
 include(VIEWS_PATH.'page.php');
 
 
