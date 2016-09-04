9ec73bd7dfc2e361b9f85a9a80a56a6b78ceb17e
Allen Garvey <allengarvey@hotmail.com>
2016-09-03 23:21:30 -0400

    Add comments to routes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ HN.settings.initSettings();
 
 var currentUrl = window.location.href;
 
+//Comments Route
 if(currentUrl.match(/\/comments\/?.*$/i)){
 	var commentsType = 'general';
 	if(currentUrl.match(/ask\/comments\/?.*$/i)){
@@ -15,9 +16,12 @@ if(currentUrl.match(/\/comments\/?.*$/i)){
 	window.addEventListener('hashchange', function(){HN.clearComments(); HN.displayComments(commentsType);}, false);
 	HN.displayComments(commentsType);
 }
+//settings route
 else if(currentUrl.match(/\/settings\/?$/i)){
 	HN.settings.initSettingsPage();
 }
+//Main category page routes - Home page, show and ask main pages
+//Home page also acts as 404, since unknown routes will end up there
 else{
 	var pageSettings = HN.pages.home;
 	if(currentUrl.match(/ask\/?$/i)){
