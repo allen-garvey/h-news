b4ba5cb4b0869d5463040bc79d6cd4f8823902ae
Allen Garvey <allen.garvey@gmail.com>
2017-12-02 20:53:13 -0500

    Add function to determine if comment is dead

--- js/comment.js
+++ js/comment.js
@@ -7,35 +7,35 @@ HN.Comment = (function(){
 
 	function HNComment(commentInfo){
 		this.commentInfo = commentInfo;
-	}
+	};
 	HNComment.prototype.isDeleted = function(){
-		if(this.commentInfo['deleted'] === true){
-			return true;
-		}
-		return false;
-	}
+		return !!this.commentInfo['deleted'];
+	};
+	HNComment.prototype.isDead = function(){
+		return !!this.commentInfo['dead'];	
+	};
 	HNComment.prototype.children = function(){
 		return this.commentInfo['kids'];
-	}
+	};
 	HNComment.prototype.parentId = function(){
 		return this.commentInfo['parent'];
-	}
+	};
 	HNComment.prototype.text = function(){
 		return this.commentInfo['text'];
-	}
+	};
 	HNComment.prototype.author = function(){
 		return this.commentInfo['by'];
-	}
+	};
 	HNComment.prototype.commentId = function(){
 		return this.commentInfo['id'];
-	}
+	};
 	
 	HNComment.prototype.numChildren = function(){
 		if(!this.children()){
 			return 0;
 		}
 		return this.children().length;
-	}
+	};
 
 	return HNComment;
 })();
