e47791f3d9331eb60ec67f1c6b1f52c34c283179
Allen Garvey <allen.garvey@gmail.com>
2019-03-17 09:51:13 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+            `${__dirname}/js/index.js`, 
+            `${__dirname}/sass/style.scss`,
+        ],
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public_html/assets'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public_html'),
+        publicPath: 'http://localhost:3000/assets/',
+        port: 3000,
+        historyApiFallback: true, //servers index.html if no route found
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            outputStyle: 'compressed',
+                        },
+                    },
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../assets/style.css',
+        }),
+    ],
+};
