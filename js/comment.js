33206d185d9f84ea71146c12c5c684b9c9f87481
Allen Garvey <allengarvey@hotmail.com>
2019-03-18 06:56:28 -0400

    Refactor comment class to use ES6 class syntax

--- js/comment.js
+++ js/comment.js
@@ -3,34 +3,37 @@
 * Used to get process info about a comment such as kids and parent id
 */
 
-export function HNComment(commentInfo){
-	this.commentInfo = commentInfo;
-};
-HNComment.prototype.isDeleted = function(){
-	return !!this.commentInfo['deleted'];
-};
-HNComment.prototype.isDead = function(){
-	return !!this.commentInfo['dead'];	
-};
-HNComment.prototype.children = function(){
-	return this.commentInfo['kids'];
-};
-HNComment.prototype.parentId = function(){
-	return this.commentInfo['parent'];
-};
-HNComment.prototype.text = function(){
-	return this.commentInfo['text'];
-};
-HNComment.prototype.author = function(){
-	return this.commentInfo['by'];
-};
-HNComment.prototype.commentId = function(){
-	return this.commentInfo['id'];
-};
-
-HNComment.prototype.numChildren = function(){
-	if(!this.children()){
-		return 0;
+export class HNComment{
+	constructor(commentInfo){
+		this.commentInfo = commentInfo;
 	}
-	return this.children().length;
-};
+
+	isDeleted(){
+		return !!this.commentInfo['deleted'];
+	};
+	isDead(){
+		return !!this.commentInfo['dead'];	
+	};
+	children(){
+		return this.commentInfo['kids'];
+	};
+	parentId(){
+		return this.commentInfo['parent'];
+	};
+	text(){
+		return this.commentInfo['text'];
+	};
+	author(){
+		return this.commentInfo['by'];
+	};
+	commentId(){
+		return this.commentInfo['id'];
+	};
+	
+	numChildren(){
+		if(!this.children()){
+			return 0;
+		}
+		return this.children().length;
+	};
+}
