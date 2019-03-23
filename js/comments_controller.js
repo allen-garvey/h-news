d589ade0c2cdc8c9164dead912a9f4f28e318213
Allen Garvey <allen.garvey@gmail.com>
2019-03-23 10:01:09 -0400

    Fix indentation

--- js/comments_controller.js
+++ js/comments_controller.js
@@ -43,68 +43,68 @@ export function displayComments(commentsType){
 };
 
 /**
-	* displays comment children of an array of comment ids
-	* if no cssId is given, assumes it is top level comment and creates appropriate jquery cssId object
-	*/
-	function displayAllCommentChildren(commentIdArray, cssId){
-		if(!commentIdArray){
-			return;
-		}
-		var isTopLevelComment = !cssId;
-		var parentList = cssId ? document.getElementById(cssId) : document.getElementById('top_list');
-		
-		commentIdArray.forEach(function(commentId){
-			const commentUrl = util.getItemInfoUrlFromId(commentId);
-			
-			getJson(commentUrl).then((commentInfo)=>{
-				displayComment(commentInfo, parentList, isTopLevelComment);
-			});
-		});
-		
-		//change hacker news links to hnews links
-		util.redirectLinks();
-
+* displays comment children of an array of comment ids
+* if no cssId is given, assumes it is top level comment and creates appropriate jquery cssId object
+*/
+function displayAllCommentChildren(commentIdArray, cssId){
+	if(!commentIdArray){
+		return;
 	}
-
-	/**
-	* displays comment
-	* parentList is a dom object that the comment should be appended to
-	* isTopLevelComment is used for styling purposes, since top level comments are styled different than child comments
-	*/
-	function displayComment(commentInfo, parentList, isTopLevelComment){
-		if(!commentInfo){
-			return;
-		}
-		const comment = new HNComment(commentInfo);
-		if(comment.isDead){
-			return;
-		}
-		let commentHTML = "<li class='comment'>";
-		if(isTopLevelComment){
-			commentHTML += "<div class='container'>";
-		}
-		commentHTML += `<h6>${comment.author}</h6><article`;
-		if(comment.isDeleted){
-			commentHTML += ' class="deleted"';	
-		}
-		commentHTML += `>${util.smartenQuotesHTML(comment.text)}</article>`;
-		
-		if(comment.numChildren > 0){
-			commentHTML += `<ol id="${commentIdToCSSId(comment.commentId)}"></ol>`;
-		}
-		if(isTopLevelComment){
-			commentHTML += '</div>';
-		}
-		commentHTML +=  '</li>';
-
-		parentList.insertAdjacentHTML('beforeend', commentHTML);
-		displayAllCommentChildren(comment.children, commentIdToCSSId(comment.commentId));
-	}
+	var isTopLevelComment = !cssId;
+	var parentList = cssId ? document.getElementById(cssId) : document.getElementById('top_list');
 	
-	/**
-	* Returns the correct jquery id cssId for the parent ol to append comment to
-	*/
-	function commentIdToCSSId(commentId){
-		return `comment${commentId}`;
+	commentIdArray.forEach(function(commentId){
+		const commentUrl = util.getItemInfoUrlFromId(commentId);
+		
+		getJson(commentUrl).then((commentInfo)=>{
+			displayComment(commentInfo, parentList, isTopLevelComment);
+		});
+	});
+	
+	//change hacker news links to hnews links
+	util.redirectLinks();
+
+}
+
+/**
+* displays comment
+* parentList is a dom object that the comment should be appended to
+* isTopLevelComment is used for styling purposes, since top level comments are styled different than child comments
+*/
+function displayComment(commentInfo, parentList, isTopLevelComment){
+	if(!commentInfo){
+		return;
 	}
+	const comment = new HNComment(commentInfo);
+	if(comment.isDead){
+		return;
+	}
+	let commentHTML = "<li class='comment'>";
+	if(isTopLevelComment){
+		commentHTML += "<div class='container'>";
+	}
+	commentHTML += `<h6>${comment.author}</h6><article`;
+	if(comment.isDeleted){
+		commentHTML += ' class="deleted"';	
+	}
+	commentHTML += `>${util.smartenQuotesHTML(comment.text)}</article>`;
+	
+	if(comment.numChildren > 0){
+		commentHTML += `<ol id="${commentIdToCSSId(comment.commentId)}"></ol>`;
+	}
+	if(isTopLevelComment){
+		commentHTML += '</div>';
+	}
+	commentHTML +=  '</li>';
+
+	parentList.insertAdjacentHTML('beforeend', commentHTML);
+	displayAllCommentChildren(comment.children, commentIdToCSSId(comment.commentId));
+}
+
+/**
+* Returns the correct jquery id cssId for the parent ol to append comment to
+*/
+function commentIdToCSSId(commentId){
+	return `comment${commentId}`;
+}
 
