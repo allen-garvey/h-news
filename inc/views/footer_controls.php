772c0370512b71acd05da58395cf1f603df7fdd7
Allen Garvey <allengarvey@hotmail.com>
2024-10-30 12:20:56 -0400

    Don't hide search button depending on page width

--- inc/views/footer_controls.php
+++ inc/views/footer_controls.php
@@ -1,4 +1,5 @@
 <a href="<?= SETTINGS_URL; ?>" class='settings_link'>Settings</a>
 <form method='GET' action='https://hn.algolia.com'>
-    <input type='search' name='query' placeholder='Search archives' /><input class='hide_for_med' type='submit' value='Search'/>
+    <input type='search' name='query' placeholder='Search archives' />
+    <input type='submit' value='Search'/>
 </form>
