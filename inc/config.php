bce2e5639c4c867b502392140c415d48fa353bce
Allen Garvey <allen.garvey@gmail.com>
2025-11-16 17:32:34 -0500

    Remove unused php constants

--- inc/config.php
+++ inc/config.php
@@ -3,8 +3,6 @@
 define('ROOT_PATH', dirname(__FILE__, 2).'/');
 define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
-define('CONTROLLERS_PATH', INC_PATH.'controllers/');
-define('MODELS_PATH', INC_PATH.'models/');
 
 define('BASE_URL','/');
 
@@ -14,10 +12,6 @@ define('SCRIPTS_URL', BASE_URL.'assets/');
 define('HOME_URL', BASE_URL);
 define('SHOW_URL', BASE_URL.'show/');
 define('ASK_URL', BASE_URL.'ask/');
-
-define('COMMENTS_QUERY_URL', BASE_URL.'comments/');
-define('ASK_COMMENTS_QUERY_URL', ASK_URL.'comments/');
-define('SHOW_COMMENTS_QUERY_URL', SHOW_URL.'comments/');
 
 define('PAGE_TITLE_DEFAULT', 'H News');
 
